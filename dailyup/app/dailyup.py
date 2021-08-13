c08471055e83a7ad0948f574a8de60ee81a24be4
san-qi <55135135+san-qi@users.noreply.github.com>
2021-08-13 21:18:03 +0800

    修复部分用户由于身份认证过期而导致填报失败的情况

--- dailyup/app/dailyup.py
+++ dailyup/app/dailyup.py
@@ -8,17 +8,35 @@ def solve(uid):
     passwd, inschool, geoinfo = sql_exec(func)
     geoinfo = loads(geoinfo)
     res = report(uid, passwd, inschool, geoinfo)
-    print(uid, ": ", res)
+    print(uid, ": ", res.text)
+    return res
+
+def is_success(res):
+    code = res.json()["e"]
+    return code!=10013
 
 def main():
     def func(cursor):
         cursor.execute("select id from user where active=1")
         return cursor.fetchall()
+    fail_user = []
     for d in sql_exec(func):
         try:
-            solve(d[0])
+            res = solve(d[0])
+            if not is_success(res):
+                fail_user.append(d[0])
         except Exception as e:
             print(e)
+    while fail_user:
+        try:
+            uid = fail_user[0]
+            fail_user = fail_user[1:]
+            res = solve(uid)
+            if not is_success(res):
+                fail_user.append(uid)
+        except Exception as e:
+            print(e)
+
 
 if __name__ == '__main__':
     main()
